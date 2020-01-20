66b51642a70655df7e747e48cba2a3aa72c96091
unknown <deejay.spory@gmail.com>
2020-01-20 11:22:41 +0100

    asd

--- processnode/Program.cs
+++ processnode/Program.cs
@@ -1,20 +1,39 @@
 ﻿using System;
 using System.Diagnostics;
 
-namespace processnode
-{
+namespace processnode { 
     class Program
     {
+        
         static void Main(string[] args)
         {
             Process[] processlist = Process.GetProcesses();
-
-            foreach (Process theprocess in processlist)
+            string ls = null;
+            string[] commands = new string[]{"ls - List all processes","q - quit"};
+            while (ls != "q")
             {
-                Console.WriteLine("Process: {0} ID: {1}", theprocess.ProcessName, theprocess.Id);
-               
+                Console.Write("User@win10> ");
+                ls = Console.ReadLine();
+                if (ls == "ls")
+                {
+                    foreach (Process theprocess in processlist)
+                    {
+                        if (theprocess.Id != 0)
+                        {
+                            Console.WriteLine("Process: {0} ID: {1} CPU: asd", theprocess.ProcessName, theprocess.Id);
+                        }
+                    }
+                    Console.WriteLine("Number of processes running: {0}", processlist.Length);
+                }
+                else
+                {
+                    for (int i = 0; i < commands.Length; i++)
+                    {
+                        Console.WriteLine(commands[i]);
+                    }
+                }
+
             }
-            Console.ReadLine();
         }
     }
 }
