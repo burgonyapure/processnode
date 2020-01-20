b068354c0e478c4ad2f8f9c457afa6bd57289f09
unknown <deejay.spory@gmail.com>
2020-01-20 09:47:41 +0100

    Created project

---
+++ processnode/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace processnode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
