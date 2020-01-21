cdff8546e5412a78522ec3ebdb5964bb685abfcb
unknown <deejay.spory@gmail.com>
2020-01-21 11:18:24 +0100

    Xml comment finished

--- processnode/Program.cs
+++ processnode/Program.cs
@@ -12,11 +12,11 @@ namespace processnode {
             string ls = null;
             string[] commands = new string[]{"ls - List all processes","q - quit"};
 
-            xml.writeXml();
             while (ls != "q")
             {
                 Console.Write("User@win10> ");
                 ls = Console.ReadLine();
+               
                 if (ls == "ls")
                 {
                     foreach (Process theprocess in processlist)
@@ -24,19 +24,48 @@ namespace processnode {
                         if (theprocess.Id != 0)
                         {
                             Console.WriteLine("Process: {0} ID: {1} MEM:{2} K", theprocess.ProcessName, theprocess.Id, Convert.ToDouble(theprocess.WorkingSet64)/100000);
+
                         }
                     }
                     Console.WriteLine("Number of processes running: {0}", processlist.Length);
-                }
+                    Console.WriteLine("Comment? (Y/N)");
+                    string ans;
+                    ans = Console.ReadLine();
+                    if (ans == "Y" || ans == "y")
+                    {
+                        string comment = null;
+                        string commentID;
+                        bool vaneID = false;
+                        Console.WriteLine("ID to comment to:");
+                        commentID = Console.ReadLine();
+
+                        string[] xmlComment = new string[4];
+
+                        foreach(Process theprocess in processlist)
+                        {
+                            if (Convert.ToString(theprocess.Id) == commentID)
+                            {
+                                vaneID = true;
+                                Console.WriteLine("Comment: ");
+                                comment = Console.ReadLine();
+                                xmlComment[0] = theprocess.ProcessName;
+                                xmlComment[1] = Convert.ToString(theprocess.Id);
+                                xmlComment[2] =(Convert.ToDouble(theprocess.WorkingSet64)/100000).ToString("N3")+" K";
+                                xmlComment[3] =comment;
+                                xml.writeXml(xmlComment);
+                                
+                            } 
+                        }
+                        if (!vaneID) { Console.WriteLine("Nincs ilyen id")};
+                    }
+                }           
                 else
                 {
                     for (int i = 0; i < commands.Length; i++)
                     {
                         Console.WriteLine(commands[i]);
                     }
-                }
-                
-
+                }               
             }
         }
     }
--- processnode/xml.cs
+++ processnode/xml.cs
@@ -2,20 +2,28 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Xml.Linq;
+using System.Xml;
 using System.IO;
 
 namespace processnode
 {
     class xml
     {
-        public static void writeXml() {
-            DateTime nev = DateTime.Now;
-            string xml_name = Convert.ToString(nev.Year) + Convert.ToString(nev.Month) + Convert.ToString(nev.Day) + Convert.ToString(nev.Hour) + Convert.ToString(nev.Minute) + Convert.ToString(nev.Second);
-            
-            StreamWriter xmlKi = new StreamWriter(@"C:\Users\teszt\source\repos\processnode\processnode\xml\"+xml_name);
-            xmlKi.WriteLine(new XElement("Process", new XAttribute("ProcessName", "/process ID/"), new XElement("Comment", "/Comment/")));
-            xmlKi.Close();
-            
+        public static void writeXml(string[] xmlComment) {
+            var filePath = @"C:\Users\teszt\source\repos\processnode\processnode\xml\comment.xml";
+            var xmlDoc = XDocument.Load(filePath);
+            var parentElement = new XElement("Process",new XAttribute(xmlComment[0],xmlComment[1]));
+            var firstNameElement = new XElement("Memory", xmlComment[2]);
+            var lastNameElement = new XElement("Comment", xmlComment[3]);
+
+            parentElement.Add(firstNameElement);
+            parentElement.Add(lastNameElement);
+
+            var rootElement = xmlDoc.Element("Processes");
+            rootElement?.Add(parentElement);
+
+            xmlDoc.Save(filePath);
+
         }
     }
 }
