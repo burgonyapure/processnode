3123ffc486a1dca1432649490c7bc17caa57c810
unknown <deejay.spory@gmail.com>
2020-01-21 09:36:45 +0100

    xml part2

--- processnode/xml.cs
+++ processnode/xml.cs
@@ -2,16 +2,20 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Xml.Linq;
+using System.IO;
 
 namespace processnode
 {
     class xml
     {
         public static void writeXml() {
-            Console.WriteLine(new XElement("Foo",new XAttribute("Bar", "some & value"),new XElement("Comment", "data")));
+            DateTime nev = DateTime.Now;
+            string xml_name = Convert.ToString(nev.Year) + Convert.ToString(nev.Month) + Convert.ToString(nev.Day) + Convert.ToString(nev.Hour) + Convert.ToString(nev.Minute) + Convert.ToString(nev.Second);
+            
+            StreamWriter xmlKi = new StreamWriter(@"C:\Users\teszt\source\repos\processnode\processnode\xml\"+xml_name);
+            xmlKi.WriteLine(new XElement("Process", new XAttribute("ProcessName", "/process ID/"), new XElement("Comment", "/Comment/")));
+            xmlKi.Close();
+            
         }
     }
 }
-<Foo Bar = "some &amp; value" >
-  < Nested > data </ Nested >
-</ Foo >
